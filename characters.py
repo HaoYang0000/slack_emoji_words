5860ad5bf90ae71afb22e0a334c135b8a62fd40f
Hao <HaoYang0000@users.noreply.github.com>
2019-02-12 00:15:57 -0600

    All english word converted

---
+++ characters.py
@@ -0,0 +1,332 @@
+class Character:
+    emoji = None
+    formatted_string = ''
+    SPACE_NUM = 6
+
+    def __init__(self, emoji: str):
+        self.emoji = emoji
+
+    def print(self) -> str:
+        """
+        Using positional and key argument to format each word.
+        position 0 is space.
+        position 1 is emoji
+        line_break is \n
+
+        :return: str
+        """
+        return self.formatted_string.format(" " * self.SPACE_NUM, self.emoji, line_break="\n")
+
+
+class A(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{1}{1}{1}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}"
+
+
+class B(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{1}{1}{1}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{1}{1}{1}{1}{1}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{1}{1}{1}{1}{0}{0}{0}{line_break}"
+
+
+class C(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{1}{1}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{1}{1}{0}{0}{line_break}"
+
+
+class D(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{1}{1}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{1}{1}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{1}{1}{0}{0}{line_break}" \
+                                "{1}{1}{1}{0}{0}{0}{0}{line_break}"
+
+
+class E(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "" \
+                                "{1}{1}{1}{1}{1}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{1}{1}{1}{1}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{1}{1}{1}{1}{1}{0}{line_break}"
+
+
+class F(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{1}{1}{1}{1}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{1}{1}{1}{1}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}"
+
+
+class G(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{1}{1}{1}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{0}{1}{1}{1}{1}{1}{line_break}" \
+                                "{1}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{1}{1}{1}{0}{0}{line_break}"
+
+
+class H(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{1}{1}{1}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}"
+
+
+class I(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{1}{1}{1}{1}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{1}{1}{1}{1}{0}{line_break}"
+
+
+class J(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{1}{1}{1}{1}{1}{line_break}" \
+                                "{0}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{1}{1}{1}{0}{0}{line_break}"
+
+
+class K(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{1}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}"
+
+
+class L(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{1}{1}{1}{1}{1}{line_break}"
+
+
+class M(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}"
+
+
+class N(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{1}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{1}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}"
+
+
+class O(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{1}{1}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{1}{1}{0}{0}{line_break}"
+
+
+class P(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{1}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{1}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}"
+
+
+class Q(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{0}{1}{1}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{0}{1}{1}{1}{0}{line_break}" \
+                                "{0}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{0}{0}{0}{1}{0}{line_break}"
+
+
+class R(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{1}{1}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{1}{1}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}"
+
+
+class S(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{0}{0}{1}{1}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{0}{0}{1}{1}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{1}{1}{0}{0}{line_break}"
+
+
+class T(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{1}{1}{1}{1}{1}{1}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}"
+
+
+class U(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{1}{1}{line_break}" \
+                                "{0}{1}{0}{0}{1}{0}{1}{line_break}" \
+                                "{0}{0}{1}{1}{0}{0}{1}{line_break}"
+
+
+class V(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}"
+
+
+class W(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{1}{0}{0}{1}{0}{0}{1}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{1}{0}{1}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}"
+
+
+class X(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{1}{0}{0}{0}{0}{0}{1}{line_break}"
+
+
+class Y(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{0}{0}{0}{0}{0}{1}{line_break}" \
+                                "{0}{1}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{1}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}"
+
+
+class Z(Character):
+    def __init__(self, emoji):
+        super().__init__(emoji=emoji)
+        self.formatted_string = "{1}{1}{1}{1}{1}{1}{1}{line_break}" \
+                                "{0}{0}{0}{0}{0}{1}{0}{line_break}" \
+                                "{0}{0}{0}{0}{1}{0}{0}{line_break}" \
+                                "{0}{0}{0}{1}{0}{0}{0}{line_break}" \
+                                "{0}{0}{1}{0}{0}{0}{0}{line_break}" \
+                                "{0}{1}{0}{0}{0}{0}{0}{line_break}" \
+                                "{1}{1}{1}{1}{1}{1}{1}{line_break}"
--- main.py
+++ main.py
@@ -1,28 +1,111 @@
-import sys
-from words import D, E, A, L, W, I, T, H
+import sys, getopt, os, platform
+from characters import *
 
-def str2Class(str):
-    return getattr(sys.modules[__name__], str)
+HELP_MESSAGE = "Please pass an emoji and a sentence to print. \n\n" \
+               "Format: python ./main.py {SLACK_EMOJI} {WORDS_OR_SENTENCE_YOU_WANT_TO_PRINT} \n\n" \
+               "For example: python ./main.py :dealwithit: DEAL with it \n\n" \
+               "To print the word in the same line, do: python ./main.py -s :dealwithit: DEAL with it \n\n" \
+               "Add flag -c to avoid the copy process(for your convenience :) " \
+               "For example: python ./main.py -c {SLACK_EMOJI} {WORDS_OR_SENTENCE_YOU_WANT_TO_PRINT} \n\n" \
+               ";-) \n"
 
-class InputTaker:
-    emoji = ""
 
-    def run(self):
-        if len(sys.argv) <= 2:
-            print ("Please pass an emoji and a sentence to print: ")
+def main(argv):
+    """
+    Main method to run, read command line argument
+    :param argv:
+    :return:
+    """
+    emoji = None
+    # If print the word in same line
+    if_same_line = False
+    # If copy to clipboard automatically
+    if_copy_to_clipboard = False
+
+    # Check if argument is enough
+    if len(argv) <= 1:
+        print(HELP_MESSAGE)
+        sys.exit(0)
+
+    # Read options
+    try:
+        opts, args = getopt.getopt(argv, "chs")
+    except getopt.GetoptError as error:
+        print(HELP_MESSAGE)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(HELP_MESSAGE)
             sys.exit(0)
-        
-        # Set up emoji to use, could be multiple
-        emoji = sys.argv[1]
+        elif opt == "-s":
+            if_same_line = True
+        elif opt == "-c":
+            if_copy_to_clipboard = True
 
-        # Append every word in array
-        for words in sys.argv[2:]:
-            # Process each character
-            for character in words:
-                class_name = character
-                word = str2Class(class_name.upper())
-                print (word(emoji=emoji).print())
-            print ("\n")
+    # Set up emoji to use, could be multiple
+    # Todo: Add validation maybe
+    emoji = args[0]
+
+    # Generate output
+    if if_same_line is False:
+        output = __process_each_character_per_line(emoji=emoji, sentence=args[1:])
+    else:
+        output = __process_each_character_same_line(emoji=emoji, sentence=args[1:])
+
+    if if_copy_to_clipboard is True:
+        add_to_clipboard(output)
+    print(output)
+
+def add_to_clipboard(output):
+    # TODO: Fix this method.
+    if platform.system() == 'Windows':
+        command = 'echo ' + output.strip() + '| clip'
+        os.system(command)
+    else:
+        os.system("echo '%s' | pbcopy" % output)
+
+def __str_to_class(class_name: str):
+    """
+    Convert string of class name to class
+    :param str:class_name
+    :return: class
+    """
+    try:
+        return getattr(sys.modules[__name__], class_name)
+    except AttributeError:
+        print("The character {character} is not implemented yet :( ".format(character=class_name))
+        return None
+
+
+def __process_each_character_per_line(emoji: str, sentence: list) -> str:
+    """
+    Process each word, and print out each character to per line
+    :param emoji:
+    :param sentence:
+    :return:
+    """
+    output = "\n"
+    # Append every word in array
+    for words in sentence:
+        # Process each character
+        for character in words:
+            class_name = character
+            word = __str_to_class(class_name.upper())
+            if word is not None:
+                output = output + word(emoji=emoji).print() + "\n"
+        output = output + "\n"
+    return output
+
+
+def __process_each_character_same_line(emoji: str, sentence: list):
+    """
+    Process each word, and print out each character for a word on the same line
+    :param emoji:
+    :param sentence:
+    :return:
+    """
+    return "This is not done yet :("
+
 
 if __name__ == "__main__":
-    InputTaker().run()
+    main(sys.argv[1:])
