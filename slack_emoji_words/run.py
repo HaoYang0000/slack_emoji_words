f6d1b051d0f14bf372117c705ae4fb3c4507dc8e
Hao Yang <hao.yang@joshs-mbp.attlocal.net>
2019-02-12 22:12:28 -0600

    Restructure project

--- slack_emoji_words/run.py
+++ slack_emoji_words/run.py
@@ -10,6 +10,26 @@ HELP_MESSAGE = "Please pass an emoji and a sentence to print. \n\n" \
                "For example: python ./main.py -c {SLACK_EMOJI} {WORDS_OR_SENTENCE_YOU_WANT_TO_PRINT} \n\n" \
                ";-) \n"
 
+def generate(
+    emoji: str = ':dealwithit:', 
+    padding: str = ':white_large_square:', 
+    input: str = 'DEAL with it', 
+    if_same_line: bool = False, 
+    if_copy_to_clipboard: bool = False, 
+    if_reverse: bool = False
+) -> str:
+    # Generate output format
+    if if_same_line is False:
+        output = process_each_character_per_line(emoji=emoji, sentence=input.split(' '))
+    else:
+        output = process_each_word_per_line(emoji=emoji, sentence=input.split(' '))
+
+    # Format output
+    output = post_format(output=output, padding=padding, emoji=emoji, if_reverse=if_reverse)
+    if if_copy_to_clipboard is True:
+        Util.add_to_clipboard(output)
+    return output
+
 
 def main(argv):
     """
